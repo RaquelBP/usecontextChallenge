45363cfdf3d0499aa62557d6f1097af4f58c5e7a
Raquel Benito <raquelbenitop@gmail.com>
2024-07-05 18:19:20 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import './App.css'; // Archivo de estilos CSS
+import React from 'react';
+import { ThemeProvider } from './themes/ThemeContext';
+//import LanguageSelector from './LanguageSelector';
+import RoutesApp from './routes/RoutesApp';
 
 const App = () => {
   return (
-<></>
+    <ThemeProvider>
+      <RoutesApp/>
+    </ThemeProvider>
   );
 };
 
